5078e4f8b7fd291fcc14208b512b24e891764eca
IshanJandaur <112973668+IshanJandaur@users.noreply.github.com>
2025-02-01 09:21:01 -0800

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import os
+from PIL import Image
+
+# Set page title
+st.set_page_config(page_title="Introduction Page", layout="centered")
+
+# Introduction
+st.markdown("<h2 style='text-align: center;'>Hi, I'm Ishan!</h2>", unsafe_allow_html=True)
+
+# Profile picture
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center; padding: 20px;">
+        <img src="imnage_path" width="200">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+col1, col2 = st.columns([1, 1])  
+
+# Resume Download Button
+with col1:
+    with open("Ishan_Jandaur_Resume.pdf", "rb") as file:
+        st.download_button(label="📄 Download Resume",
+                           data=file,
+                           file_name="Ishan_Jandaur_Resume.pdf",
+                           mime="application/pdf")
+
+# Linkedin URL Link
+with col2:
+    st.markdown("""
+        <a href="https://www.linkedin.com/in/ishanjandaur/" target="_blank">
+            <button style="
+                background-color: #0077b5; 
+                color: white; 
+                padding: 10px 20px; 
+                border: none; 
+                border-radius: 5px;
+                font-size: 16px;
+                cursor: pointer;">
+                🔗 Connect on LinkedIn
+            </button>
+        </a>
+    """, unsafe_allow_html=True)
+
+
+# Short about me 
+st.write("""
+    Hi, my name is Ishan Jandaur and I am currently a product design engineer at Lyten. In this role, I design cylindrical battery cells using Li-S chemistry.
+         
+    I was born and raised in San Jose, CA and have a bachelor's degree from California Polytechnic State University - San Luis Obispo.
+    Although my degree is in mechanical engineering, my work experience and interests have varied quite a bit. I have worked in batteries, on medical devices, and even in high volume contract manufacturing environments.
+         
+    Feel free to reach out to me on Linkedin or by email if you want to chat!
+""")
+
+# Initialize session state
+if 'counter' not in st.session_state: 
+    st.session_state.counter = 0
+
+# Function to show the next photo and cycle through images
+def show_photo():
+    # Get the current photo path based on the counter
+    photo_path = paths_images[st.session_state.counter]
+    # Open and display the image using PIL
+    photo = Image.open(photo_path)
+    col2.image(photo, caption=None)
+
+    # Increment the counter, and wrap around when it reaches the end
+    st.session_state.counter = (st.session_state.counter + 1) % len(paths_images)
+
+# Get a list of image file paths in the 'images' folder
+folder_with_images = "images"
+paths_images = [os.path.join(folder_with_images, f) for f in os.listdir(folder_with_images) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+# List out hobbies
+col1, col2 = st.columns(2)
+col1.subheader("Hobbies:")
+col1.markdown("""
+- Training my Dog
+- Collecting Vinyl Records
+- Playing Basketball
+- Traveling/Backpacking
+- Cooking New Foods
+""")
+
+# Show the button to trigger image change
+show_btn = col1.button("Click button to show pictures ⏭️", on_click=show_photo)
+
+# # Optionally show the current photo info or description
+# if st.session_state.counter < len(paths_images):
+#     st.write(f"Showing image {st.session_state.counter + 1} of {len(paths_images)}")
---
+++ pages/1_Work_Experience.py
@@ -0,0 +1,137 @@
+import streamlit as st
+
+st.set_page_config(page_title="Work Experience")
+
+# Put resume on top of page for download
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h3>Please download my resume to view in greater detail!</h3>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Button for resume download
+with open("Ishan_Jandaur_Resume.pdf", "rb") as file:
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+    """,
+        unsafe_allow_html=True,
+    )
+    st.download_button(
+        label="📄 Download Resume",
+        data=file,
+        file_name="Ishan_Jandaur_Resume.pdf",
+        mime="application/pdf",
+    )
+    st.markdown("</div>", unsafe_allow_html=True)
+
+# Experience at Lyten 
+st.markdown("### **January 2022 – Present**")
+col1, col2 = st.columns([2, 4])
+
+with col1:
+    st.image("lyten.jpg", use_container_width=True)  
+    st.markdown("**Location:** San Jose, CA")  
+    st.markdown('<p style="text-align: center;"><a href="https://lyten.com" target="_blank">Visit Website</a></p>', unsafe_allow_html=True)
+
+with col2:
+    st.markdown("#### **Product Design Engineer** | Lyten")  
+    st.write("• Spearheaded roadmap for **first 250 Wh/kg 21700 Li-S battery**")  
+    st.write("• Developed MES process flow to optimize material tracking")  
+    st.write("• Defined equipment specifications for **multi-GWh factory**")  
+    st.write("• Designed and sourced materials meeting performance & weight targets")  
+    st.write("• Led conversion of tooling & material for various battery formats")  
+    st.write("• Engineered fixtures and test methods to validate new cell designs")  
+
+    st.markdown("#### **Associate Product Design Engineer**")  
+    st.write("• Owned first 18650 & 21700 Li-S cylindrical cell designs")  
+    st.write("• Conducted **DFM, PFMEA, and DFA** on all components & assemblies")  
+    st.write("• Performed **DOEs & data analysis** for electrochemical improvements")  
+    st.write("• Scaled pilot line from **0 to 100 finished cells/shift** with **80% yield**")  
+    st.write("• Developed QC metrics & testing methods")  
+    st.write("• Led Site Acceptance Testing (**SAT**) on 3 assembly machines")  
+
+#Picture of cylindrical cells
+st.markdown("""
+    <style>
+        .centered-image {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+    <div class="centered-image">
+        <img src="lyten_1.jpg" width="200">
+    </div>
+""", unsafe_allow_html=True)
+
+#Divider
+st.markdown("""<div style="height: 5px; background-color: #69ADFF; margin: 20px 0;"></div>""", unsafe_allow_html=True)
+
+# Flex Experience
+st.markdown("### **June 2022 - August 2022**")
+col1, col2 = st.columns([2, 4])
+
+with col1:
+    st.image("flex-logo.png", width=500)  
+    st.markdown("**Location:** Milpitas, CA")  
+    st.markdown('<p style="text-align: center;"><a href="https://flex.com" target="_blank">Visit Website</a></p>', unsafe_allow_html=True)
+
+with col2:
+    st.markdown("#### **Project Engineer Intern** | Flex Inc")  
+    st.write("• Structured and scrubbed **800-line BOM** for production release")  
+    st.write("• Trained operators on complex subassembly processes")  
+    st.write("• Created & managed **in-house MES system** for inventory tracking")  
+    st.write("• Assisted PCB fabrication line by troubleshooting defects")  
+
+#Picture of Serve Robotics robot
+st.markdown("""
+    <style>
+        .centered-image {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+    <div class="centered-image">
+        <img src="serve_robotics.jpeg" width="200">
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""<div style="height: 5px; background-color: #69ADFF; margin: 20px 0;"></div>""", unsafe_allow_html=True)
+
+# Enovix Experience
+st.markdown("### **June 2021 - September 2021**")
+col1, col2 = st.columns([2, 4])
+
+with col1:
+    st.image("enovix-logo.png", width=500)  
+    st.markdown("**Location:** Fremont, CA")  
+    st.markdown('<p style="text-align: center;"><a href="https://enovix.com" target="_blank">Visit Website</a></p>', unsafe_allow_html=True)
+
+with col2:
+    st.markdown("#### **Product Engineering Intern** | Enovix Corporation")  
+    st.write("• Developed and tested next-gen lithium-ion battery with **5% energy increase**")  
+    st.write("• Led installation & qualification of Instron machine for weld pull testing")  
+    st.write("• Designed experiments to establish max allowable clamp force on cells")  
+    st.write("• Created work instructions & fixtures to streamline testing")  
+
+st.markdown("""<div style="height: 5px; background-color: #69ADFF; margin: 20px 0;"></div>""", unsafe_allow_html=True)
+
+# Toray Experience
+st.markdown("### **October 2020 - January 2021**")
+col1, col2 = st.columns([2, 4])
+
+with col1:
+    st.image("toray-logo.png", width=500)  
+    st.markdown("**Location:** Fairfield, CA")  
+    st.markdown('<p style="text-align: center;"><a href="https://toray-tac.com" target="_blank">Visit Website</a></p>', unsafe_allow_html=True)
+
+with col2:
+    st.markdown("#### **Expert/Technical Services Intern** | Toray Advanced Composites")  
+    st.write("• Communicated directly with customers on test results & specifications")  
+    st.write("• Authored technical summary reports & work instructions")  
+    st.write("• Collaborated with Quality Assurance to resolve testing discrepancies on a new DMA machine")  
+
+st.markdown("""<div style="height: 5px; background-color: #69ADFF; margin: 20px 0;"></div>""", unsafe_allow_html=True)
